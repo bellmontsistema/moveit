47fcfa1774cbbedcf3e700dc3071d9672ff1f8da
bellmontsistema <lucrorecorrentereal@gmail.com>
2021-02-26 15:00:04 -0300

    Aula de sexta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,30 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head"
 import { ChallengeBox } from "../components/ChallengeBox";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import { ChallengesProvider } from "../contexts/ChallengesContext";
+import { CountdownProvider } from "../contexts/CountdownContext";
 
 import styles from "../styles/pages/Home.module.css"
 
-export default function Home() {
+interface HomeProps {
+  level: number,
+  currentExperience: number,
+  challengesCompleted: number,
+}
+
+export default function Home(props: HomeProps) {
+
   return (
+    <ChallengesProvider 
+    level={props.level} 
+    currentExperience={props.currentExperience} 
+    challengesCompleted={props.challengesCompleted}
+    >
+    <CountdownProvider>
     <div className={styles.container}> 
 
       <Head>
@@ -29,6 +45,20 @@ export default function Home() {
       </div>
       </section>
     </div>
-
+    </CountdownProvider>
+    </ChallengesProvider>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
 }
